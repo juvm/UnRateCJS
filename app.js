c5519b280dc3f6a7b2b3e9ff5d4c5a8a6e3a7359
juvm <juveriamd01@gmail.com>
2023-05-04 20:24:58 +0530

    removed uri details

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const mongo_password = process.env['MONGO_PASSWORD'];
 
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.lyr5qsv.mongodb.net/?retryWrites=true&w=majority`;
 
-const port = process.env['PORT'] || 8000;
+const port = process.env['PORT'] || 8080;
 
 MongoClient.connect(
     uri,
